1b220aa77b6d4c97f26b9fbf5637c48884df573b
rdogm13 <drg6525@yahoo.com>
2020-03-29 12:16:43 -0700

    first commit - ui framework and mobile responsive

---
+++ scripts/nav.js
@@ -0,0 +1,70 @@
+
+const sidebarWidth = 300;
+
+var menuActive = false;
+
+
+function openSidebarMenu() {
+    $("#menu-toggle").addClass("close");
+    $("#menu-sidebar").addClass("open");
+    $("#menu-overlay").fadeIn();
+    // $("#menu-sidebar").animate({"left":"0px"}, "slow");
+    menuActive = true;
+    $('#scroll-to-top').fadeOut();
+    $('body').addClass("disable-scrolling");
+    return true;
+}
+
+function closeSidebarMenu() {
+    $("#menu-toggle").removeClass("close");
+    $("#menu-overlay").fadeOut();
+    $("#menu-sidebar").removeClass("open");
+    menuActive = false;
+    if($(window).scrollTop() > $(window).height()) {
+        $('#scroll-to-top').fadeIn();
+    }
+    $('body').removeClass("disable-scrolling");
+    return true;
+}
+
+$(document).ready(function() {
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > $(window).height() && !menuActive) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    })  
+
+    $('#scroll-to-top').click(function() {
+        $('html, body').animate({scrollTop: 0}, 700);
+        return false;
+    })
+
+
+
+    $("#menu-toggle").click(function () {
+        if (menuActive) {
+            closeSidebarMenu();
+        } else {
+            openSidebarMenu();
+        }
+    })
+
+    $("#menu-overlay").click( function() {
+        closeSidebarMenu();
+    })
+
+});
+
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    if(menuActive) {
+        closeSidebarMenu();
+    }
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 800, 'linear');
+});
