44835b626c12b61cd8c8416d17f71fee894dbbfb
rdogm13 <drg6525@yahoo.com>
2020-03-30 00:50:58 -0700

    window height test fix

--- scripts/nav.js
+++ scripts/nav.js
@@ -56,6 +56,11 @@ $(document).ready(function() {
         closeSidebarMenu();
     })
 
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    })
+
 });
 
 
