30d02caee8396837f4a03ecd7fbda05c8cf04a98
rdogm13 <drg6525@yahoo.com>
2020-03-31 20:16:22 -0700

    lightbox with description and optional gallery image added

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -5,16 +5,21 @@ const projects = [
         liveURL: "https://petstash-supply-co.herokuapp.com",
         repoURL: "https://github.com/drewgoodman/PetStash-StoreFront",
         desc: "Functional eCommerce site with account signup, searching, and checkout",
-        skills: ["React.js","SCSS","Postman"]
+        skills: ["React.js", "SCSS", "Postman"],
+        galleryImage: ["petstash-1.png"],
+        galleryText: [
+            "This is paragraph #1",
+            "Paragraph #2"    
+        ]
     },
-    
+
     {
         title: "PetStash Backend API",
         coverImage: "petstash-back2.png",
         liveURL: "https://petstash-backoffice.herokuapp.com",
         repoURL: "https://github.com/drewgoodman/PetStash-BackOfficeAPI",
         desc: "Restful API with CRUD functionality; handles the shop and user data.",
-        skills: ["Python","MySQL","Flask","Boostrap"]
+        skills: ["Python", "MySQL", "Flask", "Boostrap"]
     },
 
     {
@@ -23,18 +28,18 @@ const projects = [
         liveURL: "https://drg-triple-triad.herokuapp.com",
         repoURL: "https://github.com/drewgoodman/Triple-Triad",
         desc: "Static HTML adaptation of a mini-game from Final Fantasy VIII.",
-        skills: ["JavaScript","HTML5","CSS3"]
+        skills: ["JavaScript", "HTML5", "CSS3"]
     },
-    
+
     {
         title: "jQuery To-Do List",
         coverImage: "todo-list.png",
         liveURL: "https://drg-jquery-todo.herokuapp.com",
         repoURL: "https://github.com/drewgoodman/jQuery-ToDoList",
         desc: "Basic to-do list app with complete and delete functions.",
-        skills: ["jQuery","HTML5", "CSS3"]
+        skills: ["jQuery", "HTML5", "CSS3"]
     },
-    
+
     {
         title: "Python Games",
         coverImage: "python-games-banner.png",
@@ -42,23 +47,23 @@ const projects = [
         desc: "Hangman, Tic-tac-toe, a Magic 8-Ball, and more built to run in a terminal.",
         skills: ["Python"]
     },
-    
+
     {
         title: "eCommerce Mockup",
         coverImage: "eCommerce.png",
         liveURL: "https://drgood-ecommerce-react.herokuapp.com",
         repoURL: "https://github.com/drewgoodman/practice-ecommerce-react-app",
         desc: "Front-end React mockup of an eCommerce app for purchasing stickers.",
-        skills: ["React.js","Redux","SCSS"]
+        skills: ["React.js", "Redux", "SCSS"]
     },
-    
+
     {
         title: "Bottega Diner",
         coverImage: "diner.png",
         liveURL: "https://drg-bottega-diner.herokuapp.com",
         repoURL: "https://github.com/drewgoodman/Bottega-Diner",
         desc: "Interactive diner menu with commentary.",
-        skills: ["JavaScript","HTML5", "CSS3"]
+        skills: ["JavaScript", "HTML5", "CSS3"]
     }
 ]
 
@@ -69,13 +74,17 @@ const repoText = "Code";
 const infoIcon = '<i class="fas fa-question-circle">'
 const infoText = "Info";
 
+var lightboxOpen = false;
+
 function loadProjects() {
+    var projectIndex = 0;
     projects.forEach(project => {
-        buildProject(project);
+        buildProject(project, projectIndex);
+        projectIndex++;
     })
 }
 
-function buildProject(project) {
+function buildProject(project, projectIndex) {
     var newProject = $("<div></div>",
         {
             "class": "project-wrapper",
@@ -83,27 +92,32 @@ function buildProject(project) {
         });
 
     var projectActions = $("<div></div", { "class": "project-actions project-bg" });
-    var newInfoIcon = $("<a></a>", { "class": "project-action", "href": "#" }).append($(infoIcon), infoText)
+    var newInfoIcon = $("<a></a>", { "class": "project-action info-btn", "project-index": projectIndex }).append($(infoIcon), infoText)
     projectActions.append(buildActionIcon(project.liveURL, liveIcon, liveText), buildActionIcon(project.repoURL, repoIcon, repoText), newInfoIcon);
 
     var projectTitle = $("<div></div>", { "class": "project-title" }).append(project.title);
 
     var projectDesc = $("<div></div>", { "class": "project-desc project-bg" }).append(project.desc);
 
-    var projectSkills = $("<div></div>", { "class": "project-skills-wrapper" });
-    project.skills.forEach(skill => {
-        projectSkills.append($("<div></div>", { "class": "project-skill no-select" }).append(skill));
-    })
+    var projectSkillsWrapper =$("<div></div>", { "class": "project-skills-wrapper" });
+    appendSkills(project.skills,projectSkillsWrapper)
 
     newProject.append(
         projectActions,
         projectTitle,
-        $("<span></span>", { "class": "project-divider"}),
-        $("<span></span>", { "class": "project-shader"}),
+        $("<span></span>", { "class": "project-divider" }),
+        $("<span></span>", { "class": "project-shader" }),
         projectDesc,
-        projectSkills
+        projectSkillsWrapper
     );
     $("#projects-container").append(newProject);
+}
+
+function appendSkills(skills,newSkillsWrapper) {
+    skills.forEach(skill => {
+        newSkillsWrapper.append($("<div></div>", { "class": "project-skill no-select" }).append(skill));
+    });
+    return true;
 }
 
 function buildActionIcon(url, icon, text) {
@@ -120,7 +134,56 @@ function buildActionIcon(url, icon, text) {
     }
 }
 
+function openLightbox(projectIndex) {
+    let project = projects[projectIndex];
+    $("#lightbox-title").text(project.title)
+    let baseGalleryImg = project.galleryImage? project.galleryImage[0] : project.coverImage;
+    $("#lightbox-gallery").css("background-image",`url('../static/images/project/${baseGalleryImg}')`)
+    $("#lightbox-skills-wrapper").empty()
+    appendSkills(project.skills,$("#lightbox-skills-wrapper"));
+    $("#lightbox-text").empty();
+    if(project.galleryText) {
+        project.galleryText.forEach(paragraph => {
+            $("#lightbox-text").append($("<p></p>").text(paragraph));
+        })
+    } else if (project.desc) {
+        $("#lightbox-text").append($("<p></p>").text(project.desc));
+    } else {
+        $("#lightbox-text").append($("<p></p>").text("Project description coming soon."));
+    }
+
+    $("#lightbox").fadeIn();
+    $('body').addClass("disable-scrolling");
+    $('.unfocus-for-lightbox').addClass("blur-content");
+    $('#scroll-to-top').fadeOut();
+    setTimeout( function() {
+        lightboxOpen = true;
+    }, 500);
+}
+
+function closeLightbox() {
+    $("#lightbox").fadeOut();
+    $('body').removeClass("disable-scrolling");
+    $('.unfocus-for-lightbox').removeClass("blur-content");
+    if($(window).scrollTop() > $(window).height()) {
+        $('#scroll-to-top').fadeIn();
+    }
+    lightboxOpen = false;
+
+}
+
 $(document).ready(function () {
+
+    $(document).on('click', '.info-btn', function () {
+        let projectIndex = $(this).attr("project-index");
+        openLightbox(projectIndex);
+    })
+
+    $(".lightbox-click-close").click(function () {
+        if(lightboxOpen) {
+            closeLightbox();
+        }
+    })
 
     loadProjects();
 })
